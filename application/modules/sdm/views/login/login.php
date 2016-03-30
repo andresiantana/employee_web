7aa4d510feb02dc69413378c18df679bf7b16f2d
Miranitha Fasha <miranithafasha@gmail.com>
2016-03-31 06:18:17 +0700

    Module SDM

---
+++ application/modules/sdm/views/login/login.php
@@ -0,0 +1,29 @@
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+        <div class="panel panel-default">                            
+            <div class="panel-heading" style="text-align:center;">
+            Login Aplikasi
+        </div>
+        <div class="panel-body">
+            <?php echo form_open("sdm/login/cek_login"); ?>
+                <div class="form-group input-group">
+                    <span class="input-group-addon"><i class="fa fa-fw fa-user"></i></span>
+                    <input class="form-control" name="username" placeholder="Username">
+                </div>
+
+                <div class="form-group input-group">
+                    <span class="input-group-addon"><i class="fa fa-fw fa-key"></i></span>
+                    <input class="form-control" type="password" name="password" placeholder="Password">
+                </div>
+                <button class="button btn btn-primary btn-large">Log In</button><!-- &nbsp;<i>atau</i>&nbsp;<span><a href="<?php echo base_url('admin/login/register'); ?>">Registrasi</a></span> -->
+            <?php echo form_close(); ?>
+        </div>
+        </div>
+    </div>
+    <div class="col-md-3"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-12"></div>      
+    </div>  
+</div>
